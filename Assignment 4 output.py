5b6f9f4a0c51d25d663e59ed291bab8423e44cea
Namit21105025 <97088067+Namit21105025@users.noreply.github.com>
2022-02-26 16:52:44 +0530

    Add files via upload

---
+++ Assignment 4 output.py
@@ -0,0 +1,68 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====================== RESTART: F:\Python\Assignment 4.py ======================
+	 	 	 Assignment 4
+
+
+	 	 	 Question-1 
+
+Enter the number of discs: 3
+Move disc 1 from A to B
+Move disc 2 from A to C
+Move disc 1 from B to C
+Move disc 3 from A to B
+Move disc 1 from C to A
+Move disc 2 from C to B
+Move disc 1 from A to B
+
+
+	 	 	 Question-2 
+
+Enter number of rows: 5
+     1 
+    1 1 
+   1 2 1 
+  1 3 3 1 
+ 1 4 6 4 1 
+     
+    1 
+   1 1 
+  1 2 1 
+ 1 3 3 1 
+1 4 6 4 1 
+
+
+	 	 	 Question-3 
+
+Enter 1st number: 17
+Enter 2nd number: 3
+The quotient is 5 and the remainder is 2
+False
+False
+True
+True
+[5, 2, 4, 5, 6]
+[2, 4]
+{2, 4}
+frozenset({2, 4})
+4
+4
+
+
+	 	 	 Question-4 
+
+Student's name is NAMIT KULSHRESTHA
+Roll number is 21105025
+
+
+	 	 	 Question-5 
+
+70000
+
+
+	 	 	 Question-6 
+
+Enter George's word: letter
+Enter Barbie's word: better
+Friendship is Fake.
